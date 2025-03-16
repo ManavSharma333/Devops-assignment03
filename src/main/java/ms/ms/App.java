459db07de4455543ce3a5c2ff744847df8069c71
Manav Sharma <mshar246@my.centennialcollege.ca>
2025-03-16 02:34:33 -0400

    application is ready

---
+++ src/main/java/ms/ms/App.java
@@ -0,0 +1,13 @@
+package ms.ms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to COMP367" );
+    }
+}
